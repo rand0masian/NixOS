a03aa549612126138583e70267d5f1766371c360
rand0masian <alfieroskell@outlook.com>
2026-08-22 21:59:38 +0100

    feat(hosts): add illogical-impulse host

---
+++ modules/hosts/illogical-impulse/configuration.nix
@@ -0,0 +1,21 @@
+{ self, inputs, ... }:
+
+{
+    flake.nixosModules = {
+        illogical-impulseConfiguration = { config, ... }:
+            {
+                imports = [
+                    self.nixosModules.core
+                    self.nixosModules.illogical-impulseHardware
+                    self.nixosModules.illogical-impulseHome
+                    self.nixosModules.nvidia
+                    self.nixosModules.sddm
+                    self.nixosModules.ssh
+                    self.nixosModules.steam
+                    self.nixosModules.flatpak
+                    self.nixosModules.hyprland
+                    self.nixosModules.geoclue2
+                ];
+            };
+    };
+}
---
+++ modules/hosts/illogical-impulse/default.nix
@@ -0,0 +1,28 @@
+{ self, inputs, lib, ... }:
+
+{
+    imports = [
+        ../../options.nix
+    ];
+
+    config.flake = {
+        nixosConfigurations.illogical-impulse = inputs.nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+                {
+                    nixpkgs = {
+                        overlays = [
+                            self.overlays.neo-zen
+                            self.overlays.wallpapers
+                            self.overlays.pfps
+                        ];
+
+                        config.allowUnfree = true;
+                    };
+                }
+
+                self.nixosModules.illogical-impulseConfiguration
+            ];
+        };
+    };
+}
