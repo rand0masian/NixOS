b54af1eb89cda882541e4be8c2a6daf0f3b300e6
rand0masian <alfieroskell@outlook.com>
2026-09-01 20:56:37 +0100

    feat(hosts/illogical-impulse): add disk.nix

---
+++ modules/hosts/illogical-impulse/disk.nix
@@ -0,0 +1,46 @@
+{ self, inputs, lib, ... }:
+
+{
+    flake.nixosModules = {
+        illogical-impulseDisk = { config, ... }:
+            {
+                imports = [
+                    inputs.disko.nixosModules.disko
+                ];
+
+                disko.devices = {
+                    disk.main = {
+                        type = "disk";
+                        device = "/dev/nvme0n1";
+                        content = {
+                            type = "gpt";
+                            partitions = {
+                                ESP = {
+                                    size = "1G";
+                                    type = "EF00";
+                                    content = {
+                                        type = "filesystem";
+                                        format = "vfat";
+                                        mountpoint = "/boot";
+                                        mountOptions = [
+                                            "fmask=0077"
+                                            "dmask=0077"
+                                        ];
+                                    };
+                                };
+
+                                root = {
+                                    size = "100%";
+                                    content = {
+                                        type = "filesystem";
+                                        format = "ext4";
+                                        mountpoint = "/";
+                                    };
+                                };
+                            };
+                        };
+                    };
+                };
+            };
+    };
+}
